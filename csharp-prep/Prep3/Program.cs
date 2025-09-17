c6b4d0f5adf14b4c5dab8a062ef9d93e4f0c4395
dtrinhh <dustintrinhh@gmail.com>
2025-09-17 14:40:22 -0600

    Step 2 completion

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -15,30 +15,29 @@ class Program
         int guess = 0;
         int magicNum = 1;
 
-        if (guess != magicNum)
-        {
-            Console.WriteLine("What is the magic number?");
-            string setNum = Console.ReadLine();
-            magicNum = int.Parse(setNum);
+        Console.WriteLine("What is the magic number?");
+        string setNum = Console.ReadLine();
+        magicNum = int.Parse(setNum);
 
+        while (guess != magicNum)
+        {
 
             Console.WriteLine("What is your guess?");
             string filler = Console.ReadLine();
             guess = int.Parse(filler);
 
-        }
-
-        if (guess == magicNum)
-        {
-            Console.WriteLine("That is correct!");
-        }
-        else if (guess > magicNum)
-        {
-            Console.WriteLine("Lower");
-        }
-        else
-        {
-            Console.WriteLine("Higher");
+            if (guess == magicNum)
+            {
+                Console.WriteLine("That is correct!");
+            }
+            else if (guess > magicNum)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Higher");
+            }
         }
     }
 }
