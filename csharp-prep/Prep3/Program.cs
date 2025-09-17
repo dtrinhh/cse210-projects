dac6008be58fdc709fc9097f11f16466255399b6
dtrinhh <dustintrinhh@gmail.com>
2025-09-17 14:33:44 -0600

    Step 1 completion

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,44 @@
 using System;
 
 class Program
+// OLD CODE
+// {
+//     static void Main(string[] args)
+//     {
+//         Console.WriteLine("Hello Prep3 World!");
+//     }
+// }
+
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int guess = 0;
+        int magicNum = 1;
+
+        if (guess != magicNum)
+        {
+            Console.WriteLine("What is the magic number?");
+            string setNum = Console.ReadLine();
+            magicNum = int.Parse(setNum);
+
+
+            Console.WriteLine("What is your guess?");
+            string filler = Console.ReadLine();
+            guess = int.Parse(filler);
+
+        }
+
+        if (guess == magicNum)
+        {
+            Console.WriteLine("That is correct!");
+        }
+        else if (guess > magicNum)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("Higher");
+        }
     }
 }
