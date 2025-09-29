5536e30ca5b061518613898a422da3376eda4937
dtrinhh <dustintrinhh@gmail.com>
2025-09-29 16:50:40 -0600

    Created Resume class to display name and jobs added to my resume. (Step 6,7, and 8)

---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Resume
+{
+    public string _name;
+    public List<Job> _jobs = new List<Job>();
+
+    public void resumeDetails()
+    {
+        Console.WriteLine($"Name: {_name} ");
+        Console.WriteLine("Jobs: ");
+        foreach (Job n in _jobs)
+        {
+            Console.WriteLine($"{n._jobTitle} ");
+        }
+    }
+}
