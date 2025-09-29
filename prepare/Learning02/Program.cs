037f4564e4c067de4894fe8579c0c1ab68a39fd7
dtrinhh <dustintrinhh@gmail.com>
2025-09-29 16:52:42 -0600

    Initialized Resume class and added jobs from Job class then called for resume details. (Step 7)

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,18 +5,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Console.WriteLine("Hello Learning02 World!");
 
         Job job1 = new Job();
         job1._jobTitle = "NOC Engineer";
         job1._company = "NiCE";
         job1._startYear = 2023;
-        job1.DisplayJobDetails();
+        // job1.DisplayJobDetails();
 
         Job job2 = new Job();
         job2._jobTitle = "Dad";
         job2._company = "Home";
         job2._startYear = 2026;
-        job2.DisplayJobDetails();
+        // job2.DisplayJobDetails();
+
+        Resume resume1 = new Resume();
+        resume1._name = "Dustin Trinh";
+        // Console.WriteLine("Jobs: ");
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        resume1.resumeDetails();
     }
 }
