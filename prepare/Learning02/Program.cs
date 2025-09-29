c2e853f5763cdd32c102b818f7a69bf0be4e7da5
dtrinhh <dustintrinhh@gmail.com>
2025-09-29 13:52:22 -0600

    Initialized Job class for 2 new job entries containing title, company, start year. called for member function to display job details (step 4)

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Collections.Specialized;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        job1._jobTitle = "NOC Engineer";
+        job1._company = "NiCE";
+        job1._startYear = 2023;
+        job1.DisplayJobDetails();
+
+        Job job2 = new Job();
+        job2._jobTitle = "Dad";
+        job2._company = "Home";
+        job2._startYear = 2026;
+        job2.DisplayJobDetails();
     }
 }
