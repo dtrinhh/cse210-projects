5f3726068612b4de75b357c4cb2f7d81ef1afba7
dtrinhh <dustintrinhh@gmail.com>
2025-10-16 13:12:30 -0600

    Step 5 completion

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Dynamic;
 
 class Program
 {
@@ -12,5 +13,12 @@ class Program
         int testNum2 = 7;
         Fraction fractionTest3 = new Fraction(testNum1, testNum2);
 
+        fractionTest1.SetTop(1000);
+
+        Console.WriteLine(fractionTest1.GetTop());
+
+        fractionTest1.SetBottom(2000);
+
+        Console.WriteLine(fractionTest1.GetBottom());
     }
 }
