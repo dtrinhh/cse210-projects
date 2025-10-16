67ee7d312174d125f2ac50a5a35c2b19d3360d40
dtrinhh <dustintrinhh@gmail.com>
2025-10-16 12:37:37 -0600

    Step 1-4 completion

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+
+class Fraction
+{
+    private int _top;
+
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+        int fractionTotal = _top / _bottom;
+
+        Console.WriteLine($"Your total is {fractionTotal} (numerator and denomintor default to 1)");
+    }
+
+    public Fraction(int top)
+    {
+        _top = top;
+        _bottom = 1;
+        int fractionTotal = _top / _bottom;
+
+        Console.WriteLine($"Your total is {fractionTotal} (denomintor default to 1)");
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+
+        // Casting one of the int variables to be a double type because it is a requirement. Otherwise just returns the integer portion and no decimals.
+        double fractionTotal = _top / (double)_bottom;
+
+        // Displays Total with 2 decimal places (rounded) You can select ho wmany it'll round to by changing the number in .ToString("F#")
+        string cleanTotal = fractionTotal.ToString("F2");
+        Console.WriteLine($"Your total is {cleanTotal} (numerator = {_top} & Denominator = {_bottom})");
+
+
+        // Displays Total with ALL decimals
+        // Console.WriteLine($"Your total is {fractionTotal} (numerator = {_top} & Denominator = {_bottom})");
+    }
+
+
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fractionTest1 = new Fraction();
+
+        int testNum1 = 6;
+        Fraction fractionTest2 = new Fraction(testNum1);
+
+        int testNum2 = 7;
+        Fraction fractionTest3 = new Fraction(testNum1, testNum2);
+
     }
 }
