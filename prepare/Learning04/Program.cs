269442587ea576638a55c2f7cbd959cc7f1a6ba3
dtrinhh <dustintrinhh@gmail.com>
2025-10-30 22:08:40 -0600

    Completion of Step 1 - 5 in w7  Inheritance learning activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        string name = "Dustin Trinh";
+        string topic = "Addition";
+        string textbookSelection = "Mathematics Vol 1";
+        string problems = "1-10";
+        string title = "Math for Dummies";
+
+
+        Assignment assignment1 = new Assignment(name, topic);
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment mathAssignment1 = new MathAssignment(name, topic, textbookSelection, problems);
+        Console.Write(mathAssignment1.GetSummary());
+        Console.WriteLine(mathAssignment1.GetHomeworkList());
+
+        WritingAssignment writingAssignment1 = new WritingAssignment(name, topic, title);
+        Console.Write(writingAssignment1.GetSummary());
+        Console.WriteLine(writingAssignment1.GetWritingInformation());
     }
 }
